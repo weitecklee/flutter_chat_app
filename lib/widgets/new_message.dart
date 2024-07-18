9f4bf95b2a21b401addf9a09cb3e47240d2cb86c
arkteck <arkteck@gmail.com>
2024-07-18 11:22:43 -0400

    store messages in firestore

---
+++ lib/widgets/new_message.dart
@@ -0,0 +1,78 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class NewMessage extends StatefulWidget {
+  const NewMessage({super.key});
+
+  @override
+  State<NewMessage> createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  final _messageController = TextEditingController();
+  var _enableButton = false;
+
+  @override
+  void dispose() {
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  void _submitMessage() async {
+    final enteredMessage = _messageController.text;
+    FocusScope.of(context).unfocus();
+    setState(() {
+      _enableButton = false;
+    });
+    _messageController.clear();
+
+    final user = FirebaseAuth.instance.currentUser!;
+    final userData = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(user.uid)
+        .get();
+
+    FirebaseFirestore.instance.collection('chat').add({
+      'message': enteredMessage,
+      'createdAt': Timestamp.now(),
+      'userId': user.uid,
+      'username': userData.data()!['username'],
+      'userImage': userData.data()!['image_url'],
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 15,
+        right: 1,
+        bottom: 14,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _messageController,
+              textCapitalization: TextCapitalization.sentences,
+              autocorrect: true,
+              enableSuggestions: true,
+              decoration: const InputDecoration(labelText: 'Send a message...'),
+              onChanged: (value) {
+                setState(() {
+                  _enableButton = value.trim().isNotEmpty;
+                });
+              },
+            ),
+          ),
+          IconButton(
+            color: Theme.of(context).colorScheme.primary,
+            onPressed: _enableButton ? _submitMessage : null,
+            icon: const Icon(Icons.send),
+          ),
+        ],
+      ),
+    );
+  }
+}
