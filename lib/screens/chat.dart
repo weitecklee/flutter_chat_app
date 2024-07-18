763faf23f3ebbb4d9910ba7cb1cf6f82a62a59f3
arkteck <arkteck@gmail.com>
2024-07-18 15:06:55 -0400

    push notifications on android working

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -17,8 +17,7 @@ class _ChatScreenState extends State<ChatScreen> {
 
     await fcm.requestPermission();
 
-    final token = await fcm.getToken();
-    print(token);
+    fcm.subscribeToTopic('chat');
   }
 
   @override
